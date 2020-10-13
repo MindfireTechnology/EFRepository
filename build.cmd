74e9af6fea125e60b3ef09368d77d3cd0d833fdc
Nate Zaugg <Nate@MindfireTechnology.com>
2020-10-13 17:55:17 -0600

    Working on refactor for EFRepository

--- build.cmd
+++ build.cmd
@@ -1,2 +1,5 @@
-dotnet pack src/EFRepository-Core-3_0/EFRepository-Core-3_0.csproj -p:NuspecFile=../EFRepository/Mindfire.EFRepository.nuspec --output ./releases -c Release
-timeout 10
+pushd src
+msbuild EFRepository.sln -p:Configuration=Release --verbosity:quiet
+
+dotnet pack EFRepository-Core-3_0/EFRepository-Core-3_0.csproj --include-symbols -p:NuspecFile=../EFRepository/Mindfire.EFRepository.nuspec --output ../Releases -c Release
+popd
